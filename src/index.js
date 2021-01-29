4b4f8b65c908a99f909f3f2a9f69e813d6a91dcb
ngwatso <ngwatson79@yahoo.com>
2021-01-29 10:10:02 -0500

    set up initial state, actions, feature reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
+import { featuresReducer } from "./reducers/featuresReducer";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// !! createStore
+const store = createStore(featuresReducer);
+
+// !! Wrap App in Provider
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
