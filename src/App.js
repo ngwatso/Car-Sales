a906c5f7782bc2f2effc84af56a6b62b26a09fd4
ngwatso <ngwatson79@yahoo.com>
2021-01-31 16:49:10 -0500

    starting new branch, too many errors

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
-import featuresReducer from "./reducers/featuresReducer";
+import { featuresReducer } from "./reducers/featuresReducer";
 
 const App = (props) => {
-	// const state = {
-	// 	additionalPrice: 0,
-	// 	car: {
-	// 		price: 26395,
-	// 		name: "2019 Ford Mustang",
-	// 		image:
-	// 			"https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-	// 		features: [],
-	// 	},
+	const state = {
+		additionalPrice: 0,
+		car: {
+			price: 26395,
+			name: "2019 Ford Mustang",
+			image:
+				"https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+			features: [],
+		},
 	// 	additionalFeatures: [
 	// 		{ id: 1, name: "V-6 engine", price: 1500 },
 	// 		{ id: 2, name: "Racing detail package", price: 1500 },
@@ -27,16 +27,16 @@ const App = (props) => {
 	return (
 		<div className="boxes">
 			<div className="box">
-				<Header car={props.car} />
-				<AddedFeatures car={props.car} />
+				<Header car={state.car} />
+				<AddedFeatures car={state.car} />
 			</div>
 			<div className="box">
 				<AdditionalFeatures
-					additionalFeatures={props.additionalFeatures}
+					additionalFeatures={state.additionalFeatures}
 				/>
 				<Total
-					car={props.car}
-					additionalPrice={props.additionalPrice}
+					car={state.car}
+					additionalPrice={state.additionalPrice}
 				/>
 			</div>
 		</div>
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const AdditionalFeature = props => {
+
+  const handleAddFeature = (e) => {
+    e.preventDefault();
+    this.props.addFeature(this.state.)
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
